5ee9e2c571326971484be15d383886ada4cb5f42
notRyoros <marcel.iwastschenko@freenet.de>
2025-05-25 23:49:52 +0200

    creating project

---
+++ src/client/java/mod/rat_pack_studios/fantastic_worlds/client/FantasticWorldsDataGenerator.java
@@ -0,0 +1,12 @@
+package mod.rat_pack_studios.fantastic_worlds.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FantasticWorldsDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
---
+++ src/main/java/mod/rat_pack_studios/fantastic_worlds/FantasticWorlds.java
@@ -0,0 +1,10 @@
+package mod.rat_pack_studios.fantastic_worlds;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FantasticWorlds implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
