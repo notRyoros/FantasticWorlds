9a952115f1238c0d105a25d1dd47644326ab1f6b
notRyoros <marcel.iwastschenko@freenet.de>
2025-06-06 21:34:47 +0200

    Fixing the biome related Problems, where Oceans did not generate correctly Fixing the lava level related bug
    Slight overhaul of the plains.json
    Slight overhaul of the taiga.json, snowy_taiga.json, old_growth_pine_taiga.json, old_growth_spruce_taiga.json

--- src/main/java/mod/rat_pack_studios/fantastic_worlds/mixin/NoiseBasedChunkGeneratorMixin.java
+++ src/main/java/mod/rat_pack_studios/fantastic_worlds/mixin/NoiseBasedChunkGeneratorMixin.java
@@ -1,22 +1,11 @@
 package mod.rat_pack_studios.fantastic_worlds.mixin;
 
-import com.mojang.serialization.MapCodec;
-import mod.rat_pack_studios.fantastic_worlds.WorldPresetKeys;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.server.packs.resources.Resource;
-import net.minecraft.tags.WorldPresetTags;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.dimension.BuiltinDimensionTypes;
 import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.levelgen.Aquifer;
 import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
 import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
-import net.minecraft.world.level.levelgen.presets.WorldPresets;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -24,18 +13,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(NoiseBasedChunkGenerator.class)
 public class NoiseBasedChunkGeneratorMixin {
 
-    /**
-     * @author
-     * @reason
-     */
-    @Overwrite
-    private static Aquifer.FluidPicker createFluidPicker(NoiseGeneratorSettings p_249264_) {
-        Aquifer.FluidStatus $$1 = new Aquifer.FluidStatus(-114, Blocks.LAVA.defaultBlockState());
-        int $$2 = p_249264_.seaLevel();
-        Aquifer.FluidStatus $$3 = new Aquifer.FluidStatus($$2, p_249264_.defaultFluid());
-        Aquifer.FluidStatus $$4 = new Aquifer.FluidStatus(DimensionType.MIN_Y * 2, Blocks.AIR.defaultBlockState());
-        return (p_224274_, p_224275_, p_224276_) -> {
-            return p_224275_ < Math.min(-114, $$2) ? $$1 : $$3;
-        };
+    @Inject(method = "createFluidPicker", at = @At("HEAD"), cancellable = true)
+    private static void overwriteLavaLevel(NoiseGeneratorSettings p_249264_, CallbackInfoReturnable<Aquifer.FluidPicker> cir)
+    {
+        Aquifer.FluidStatus aquifer$fluidstatus = new Aquifer.FluidStatus(-114, Blocks.LAVA.defaultBlockState());
+        int i = p_249264_.seaLevel();
+        Aquifer.FluidStatus aquifer$fluidstatus1 = new Aquifer.FluidStatus(i, p_249264_.defaultFluid());
+        Aquifer.FluidStatus aquifer$fluidstatus2 = new Aquifer.FluidStatus(DimensionType.MIN_Y * 2, Blocks.AIR.defaultBlockState());
+        cir.setReturnValue((p_224274_, p_224275_, p_224276_) -> {
+            return p_224275_ < Math.min(-114, i) ? aquifer$fluidstatus : aquifer$fluidstatus1;
+        });
     }
 }
